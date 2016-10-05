5021d276c64a1af83f70c35115204fd5702a0e18
vipinbalan <ksvipinbalan@gmail.com>
2016-10-05 19:40:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/article-one', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-one', function (req, res) {
+app.get('/article-two', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
